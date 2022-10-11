3f3bbf3118e5c4dc57f2a4da6c89faf00ce7142a
silvanda@arcada.fi <silvanda@arcada.fi>
2022-10-11 16:20:11 +0300

    commit

--- routes/cabins.js
+++ routes/cabins.js
@@ -1,35 +1,59 @@
 const express = require('express')
-const http = require('http')
+const https = require('https')
+//const fetch = require('node-fetch')
 const authToken = require('../middleware/authToken')
 const cabin = require('../models/cabin')
 const router = express.Router()
 const Cabin = 'https://schoolproject2.azurewebsites.net/cabins'
-const options = {
-    hostname: 'http://schoolproject2.azurewebsites.net',
-    path: '/cabins',
-    method: 'GET'
-};
 
-router.get('/', authToken, async (req, res)=>{
+router.get('/', authToken, async (req, res) => {
+    // const users = await User.find()
+
+    // console.log(req)
+let url = "schoolproject2.azurewebsites.net";
+var options = {
+    hostname: url,
+    port: 443,
+    path: '/cabins',
+    method: 'GET',
+    headers:{
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MzMyYWUwNzkyNWYzODI1NTdlYTAyZTgiLCJlbWFpbCI6ImpvaG4uZG9lQGFyY2FkYS5maSIsImlhdCI6MTY2NTQ5MjY3NiwiZXhwIjoxNjY1NTc5MDc2fQ.Zrl4fSgUTEQ-Jq4n82lQF_fNyu-yyDhpfk9c4tyb-Qs',
+
+   }
+};
+https.get(options,(res) => {
+    let body = ""
+
+    res.on("data", (chunk) => {
+        body += chunk
+    });
+
+    res.on("end", () => {
+        try {
+             let json = JSON.parse(body)
+            // do something with JSON
+
+            // console.log(json)
+        } catch (error) {
+            console.error(error.message)
+        };
+    });
+
+}).on("error", (error) => {
+    console.error(error.message);
+});
+res.send(json);
+})
+
+// den gettar från fel server!
+/*https.get('https://schoolproject2.azurewebsites.net/cabins',
+ authToken, async (req, res)=>{
     try{
-        const request = http.request(options, (res) => {
-            let data = ''
-        
-            res.on('data', (chunk) => {
-                data += chunk;
-            });
-            // Ending the response 
-            res.on('end', () => {
-                console.log('Body:', JSON.parse(data))
-            });
-        
-        }).on("error", (err) => {
-            console.log("Error: ", err)
-        }).end()
-        return res.send('ok')
+        const cabins = await cabin.find();
+        res.send(cabins);
     }catch(error){
         res.status(500).send({msg: error.message})
     }
-})
+})*/
 
 module.exports = router;
