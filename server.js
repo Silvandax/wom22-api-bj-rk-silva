26d27d2b9f4eee34c629d9bd83117e62e70231fb
silvanda@arcada.fi <silvanda@arcada.fi>
2022-10-16 14:33:59 +0300

    semicolon style fix

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const PORT = process.env.PORT || 3030;
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const PORT = process.env.PORT || 3030
 
-require('dotenv').config();
+require('dotenv').config()
 
-console.log(process.env.DOTENV_WORKS);
-mongoose.connect(process.env.DB_URL);
-const db = mongoose.connection;
-db.on('error', (error) => console.log(error));
-db.on('open', () => console.log('Connected to DB'));
+console.log(process.env.DOTENV_WORKS)
+mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.on('open', () => console.log('Connected to DB'))
 
 
-const cabinsRouter = require('./routes/cabins');
-const ordersRouter = require('./routes/orders');
-const servicesRouter = require('./routes/services');
+const cabinsRouter = require('./routes/cabins')
+const ordersRouter = require('./routes/orders')
+const servicesRouter = require('./routes/services')
 
 // för att ta emot json
-app.use(express.json());
+app.use(express.json())
 
-app.use('/cabins', cabinsRouter);
-app.use('/orders', ordersRouter);
-app.use('/services', servicesRouter);
+app.use('/cabins', cabinsRouter)
+app.use('/orders', ordersRouter)
+app.use('/services', servicesRouter)
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
